a5157b94edee2bc7c2cbefec98e67d7b64dae599
HelloHang <544533631@qq.com>
2019-08-22 23:02:40 +0800

    Update rest template call

--- productService/src/main/java/com/product/ehcache/CustomCacheLoaderWriter.java
+++ productService/src/main/java/com/product/ehcache/CustomCacheLoaderWriter.java
@@ -1,67 +1,70 @@
 package com.product.ehcache;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.product.entity.ProductEntity;
+import java.util.Map;
 import org.ehcache.spi.loaderwriter.BulkCacheLoadingException;
 import org.ehcache.spi.loaderwriter.BulkCacheWritingException;
 import org.ehcache.spi.loaderwriter.CacheLoaderWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
 
+public class CustomCacheLoaderWriter implements CacheLoaderWriter {
 
-public class CustomCacheLoaderWriter implements CacheLoaderWriter
-{
-	private RestTemplate restTemplate = new RestTemplate();
+  public static final String EMPTY_OBJECT = "{}";
+  private RestTemplate restTemplate = new RestTemplate();
 
-	private ObjectMapper objectMapper = new ObjectMapper();
+  private ObjectMapper objectMapper = new ObjectMapper();
 
-	private static final Logger LOG = LoggerFactory.getLogger(CustomCacheLoaderWriter.class);
+  private static final Logger LOG = LoggerFactory.getLogger(CustomCacheLoaderWriter.class);
 
-	@Override
-	public Map loadAll(Iterable keys) throws BulkCacheLoadingException, Exception
-	{
-		return null;
-	}
+  @Override
+  public Map loadAll(Iterable keys) throws BulkCacheLoadingException, Exception {
+    return null;
+  }
 
-	@Override
-	public void writeAll(Iterable iterable) throws BulkCacheWritingException, Exception
-	{
+  @Override
+  public void writeAll(Iterable iterable) throws BulkCacheWritingException, Exception {
 
-	}
+  }
 
-	@Override
-	public void deleteAll(Iterable keys) throws BulkCacheWritingException, Exception
-	{
+  @Override
+  public void deleteAll(Iterable keys) throws BulkCacheWritingException, Exception {
 
-	}
+  }
 
-	@Override
-	public Object load(Object key) throws Exception
-	{
-		LOG.info("Get data form custom cache.");
-		String value = restTemplate.getForObject("http://local:host:8002/redis?Key={?}", String.class, key);
-		if (value != null)
-		{
-			return objectMapper.readValue(value, ProductEntity.class);
-		}
-		return null;
-	}
+  @Override
+  public Object load(Object key) throws Exception {
+    LOG.info("Get data form custom cache.");
+    String value = restTemplate
+        .getForObject("http://localhost:8002/redis?key={?}", String.class, key);
+    if (value != null && !EMPTY_OBJECT.equals(value)) {
+      return objectMapper.readValue(value, ProductEntity.class);
+    }
+    return null;
+  }
 
-	@Override
-	public void write(Object key, Object value) throws Exception
-	{
-		LOG.info("Writer data to custom cache.");
-		restTemplate.put("http://local:host:8002/redis?Key={?}&&value={?}", key, value);
-		LOG.info("Writer data to custom cache successfully.");
-	}
+  @Override
+  public void write(Object key, Object value) throws Exception {
+    objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    LOG.info("Writer data to custom cache.");
+    final String json = objectMapper.writeValueAsString(value);
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
+    HttpEntity<String> httpEntity = new HttpEntity<>(json, httpHeaders);
+    restTemplate.put("http://localhost:8002/redis?key={?}", httpEntity, key);
+    LOG.info("Writer data to custom cache successfully.");
+  }
 
-	@Override
-	public void delete(Object key) throws Exception
-	{
-		restTemplate.delete("http://local:host:8002/redis?Key={?}", key);
-		LOG.info("delete data form redis successful.");
-	}
+  @Override
+  public void delete(Object key) throws Exception {
+    restTemplate.delete("http://local:host:8002/redis?Key={?}", key);
+    LOG.info("delete data form redis successful.");
+  }
 }
--- redis/src/main/java/com/ecommerce/redis/controller/RedisController.java
+++ redis/src/main/java/com/ecommerce/redis/controller/RedisController.java
@@ -4,46 +4,44 @@ import com.ecommerce.redis.service.RedisService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/redis")
-public class RedisController
-{
-	@Autowired
-	private RedisService redisService;
+public class RedisController {
 
-	@ApiOperation(value = "Put value to redis")
-	@ApiImplicitParams({
-		  @ApiImplicitParam(name = "key", value = "Key", required = true, dataType = "String"),
-		  @ApiImplicitParam(name = "value", value = "Value", required = true, dataType = "String")
-	})
-	@PutMapping
-	public void putValueToRedis(@RequestParam(value = "key") String key, @RequestParam(value = "value") String value)
-	{
-		redisService.put(key,value);
-	}
+  @Autowired
+  private RedisService redisService;
 
-	@ApiOperation(value = "Get value to redis")
-	@ApiImplicitParams({
-		  @ApiImplicitParam(name = "key", value = "Key", required = true, dataType = "String")
-	})
-	@GetMapping
-	public String getValueFromRedis(@RequestParam(value = "key") String key)
-	{
-		return redisService.get(key);
-	}
+  @ApiOperation(value = "Put value to redis")
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "key", value = "Key", required = true, dataType = "String")
+  })
+  @PutMapping
+  public void putValueToRedis(@RequestParam(value = "key") String key,
+      @RequestBody @ApiParam(name = "value", value = "Value", required = true) String value) {
+    redisService.put(key, value);
+  }
 
-	@ApiOperation(value = "Delete value to redis")
-	@ApiImplicitParams({
-		  @ApiImplicitParam(name = "key", value = "Key", required = true, dataType = "String")
-	})
-	@DeleteMapping
-	public void deleteValueFormRedis(@RequestParam(value = "key") String key)
-	{
-		redisService.delete(key);
-	}
+  @ApiOperation(value = "Get value to redis")
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "key", value = "Key", required = true, dataType = "String")
+  })
+  @GetMapping
+  public String getValueFromRedis(@RequestParam(value = "key") String key) {
+    return redisService.get(key);
+  }
+
+  @ApiOperation(value = "Delete value to redis")
+  @ApiImplicitParams({
+      @ApiImplicitParam(name = "key", value = "Key", required = true, dataType = "String")
+  })
+  @DeleteMapping
+  public void deleteValueFormRedis(@RequestParam(value = "key") String key) {
+    redisService.delete(key);
+  }
 
 }
